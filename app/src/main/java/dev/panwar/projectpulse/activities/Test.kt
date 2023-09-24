99d37b6c3840fcfefb4882423a7d2df53d600d88
Dev Panwar <devpanwar9784@gmail.com>
2023-09-24 19:45:20 +0530

    test added

---
+++ app/src/main/java/dev/panwar/projectpulse/activities/Test.kt
@@ -0,0 +1,11 @@
+package dev.panwar.projectpulse.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Test : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
